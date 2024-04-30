30732587053e683db9cc8f2015aa7d16fff3a98f
Rajashree0316 <rajashreeasok16@gmail.com>
2024-04-30 14:44:41 +0530

    first commit

---
+++ src/components/context.js
@@ -0,0 +1,55 @@
+import React, { useState, useContext } from 'react';
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isModalOpen,
+        openModal,
+        closeModal,
+        openSidebar,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// custom hook
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
+
+
+/* things to know
+1)Access children in the component
+2)custom hook returns that coming from react useContext and AppContext
+3) Either we import useContext and AppContext in each component nor
+import useGlobalContext
+4)Note: need to have this "use" in custom hook otherwise we get error
+coz we are calling useContext one of the react hook
+5)In value, we can pass useState values and functions
+*/
